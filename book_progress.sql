70001b476de589aa49b39b67aab742b2a28c25db
jimvic37@github.com <Ethan>
2023-07-25 13:43:52 -0500

    Added more dummy data

--- book_progress.sql
+++ book_progress.sql
@@ -37,7 +37,12 @@ values
 ("Sarah", "Johnson", "sarahj", "pass456"),
 ("Michael", "Williams", "mike22", "secret321"),
 ("Emily", "Brown", "emilyb", "abcdefg123"),
-("Daniel", "Davis", "danny_d", "qwerty789");
+("Daniel", "Davis", "danny_d", "qwerty789"),
+("Jessica", "Wilson", "jessy22", "p@ssw0rd"),
+("David", "Anderson", "david_and", "dA12345"),
+("William", "Thompson", "willT91", "p@ssw0rd!"),
+("Sophia", "Scott", "sophiaS", "Ss34567"),
+("Olivia", "Martinez", "o_mart", "oliviaM2023");
 
 insert into books(title, author_first_name, author_last_name, num_of_pages) 
 values
@@ -46,7 +51,11 @@ values
 ("1984", "George", "Orwell", 328),
 ("Pride and Prejudice", "Jane", "Austen", 279),
 ("The Catcher in the Rye", "J.D.", "Salinger", 234),
-("The Intelligent Investor", "Benjamin", "Graham", 640);
+("The Intelligent Investor", "Benjamin", "Graham", 640),
+("Harry Potter and the Sorcerer's Stone", "J.K.", "Rowling", 320),
+("The Hobbit", "J.R.R.", "Tolkien", 310),
+("The Lord of the Rings", "J.R.R.", "Tolkien", 1178),
+("Brave New World", "Aldous", "Huxley", 288);
 
 insert into users_books(user_id, book_id, pages_read) 
 values
@@ -58,7 +67,12 @@ values
 (3, 2, 20),
 (4, 3, 328),
 (5, 2, 0),
-(5, 4, 200);
+(5, 4, 200),
+(7, 7, 100),
+(10, 5, 0),
+(9, 9, 600),
+(8, 1, 100);
+
 
 select * from users;
 select * from books;
